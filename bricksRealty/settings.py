edc1fd1402c63f2b7e7cb7a9919b204bc828093c
Mwangik <gkmwangi0@gmail.com>
2019-03-13 02:18:50 +0300

    create listings models

--- bricksRealty/settings.py
+++ bricksRealty/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'bricksRealty.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bricksrealty',
+        'USER': 'postgres',
+        'PASSWORD': '5029',
+        'HOST': 'localhost'
     }
 }
 
