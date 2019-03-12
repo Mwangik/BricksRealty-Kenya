9c7daba479b61d71edec5f8d424e636439bcc265
Mwangik <gkmwangi0@gmail.com>
2019-03-13 02:34:46 +0300

    create realtors models and run migrations

--- listings/models.py
+++ listings/models.py
@@ -13,8 +13,8 @@ class Listing(models.Model):
     description = models.TextField(blank = True)
     price = models.IntegerField()
     bedrooms = models.IntegerField()
-    bathrooms = models.DecimalField(max_digits=2, decimapl_places = 1)
-    garage = models.IntegerField(default = 0)
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
@@ -25,6 +25,6 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(default =datetime.now, blank = True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.title
