0dcbb6524c086102500352dc6b6a3992ac1fc16e
Mwangik <gkmwangi0@gmail.com>
2019-03-18 18:13:25 +0300

    add functionality to send email to realtor and customize user dashboard

--- bricksRealty/settings.py
+++ bricksRealty/settings.py
@@ -141,3 +141,10 @@ MESSAGE_TAGS = {
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'gitaukelvin7@gmail.com'
+EMAIL_HOST_PASSWORD = 'monster1995'
+EMAIL_USE_TLS=True
